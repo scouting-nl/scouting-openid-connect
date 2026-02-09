ae3528939d413c6734da15824dfc8a442f7d840c
Job van Koeveringe <developer@jobvankoeveringe.com>
2026-02-09 22:41:19 +0100

    Adjust padding in custom redirect display for improved layout

--- src/settings/general.php
+++ src/settings/general.php
@@ -352,7 +352,7 @@ class Settings_General
         if (!empty($value) && strpos($value, $base_domain) === 0) {
             $slug = substr($value, strlen($base_domain));
         }
-        echo '<span style="padding: 5.675px 0px;">' . esc_html($base_domain) . '</span>';
+        echo '<span style="padding: 5.675px 3px 5.675px 0px;">' . esc_html($base_domain) . '</span>';
         echo '<input type="text" id="scouting_oidc_custom_redirect" name="scouting_oidc_custom_redirect" size="50" value="' . esc_attr($slug) . '" placeholder="' . esc_attr__("custom-page", "scouting-openid-connect") . '"/>';
         echo '<p class="description">' . esc_html__("Enter the slug to append to the base URL where users should be redirected after login.", "scouting-openid-connect") . '</p>';
     }
