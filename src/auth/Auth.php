d5f827df1afbe81d3102a6e4f08eb954567acebe
Job van Koeveringe <job.van.koeveringe@prodrive-technologies.com>
2026-08-03 10:25:59 +0200

    Clean OIDC callback parameters and redirect to home after login

--- src/auth/Auth.php
+++ src/auth/Auth.php
@@ -295,11 +295,15 @@ class Auth {
                 ErrorHandler::redirect_to_login_error('error', __('Webmaster disabled creation of new accounts', 'scouting-openid-connect'), 'disabled_auto_create');
             }
         }
+
+        // Configured post-login redirects exit from the wp_login hook. Clean OIDC callback parameters for the default flow.
+        wp_safe_redirect(home_url('/'));
+        exit;
     }
 
     /**
      * Callback after failed login
-     * 
+     *
      * @param string $message the error message
      * @return string the HTML for the error message or an empty string if the user is not logged in
      */
